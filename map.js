46abe41a65c6e08f1d1ee8776ffc7320b22f7ec8
GDS-Verint <45266375+GDS-Verint@users.noreply.github.com>
2018-12-10 14:18:25 +0000

    Added isPointInPolygon

--- map.js
+++ map.js
@@ -703,3 +703,22 @@ function addLocate(map, gisContainer){
 		}
 	);
 }
+function isPointInPolygon(point, polygon){
+	//KS: must be same wkid
+	//KS e.g. isPointInPolygon([1.5,1.5],[[1,1],[1,2],[2,2],[2,1]]);
+	// ray-casting algorithm based on
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+
+    var x = point[0], y = point[1];
+
+    var inside = false;
+    for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+        var xi = polygon[i][0], yi = polygon[i][1];
+        var xj = polygon[j][0], yj = polygon[j][1];
+
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
+}
