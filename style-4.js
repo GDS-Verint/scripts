ac152d3d71aa19269b5162c8b4ef85ad4eecb3aa
GDS-Verint <45266375+GDS-Verint@users.noreply.github.com>
2019-01-29 09:14:58 +0000

    Update style-4.js

--- style-4.js
+++ style-4.js
@@ -530,3 +530,17 @@ function paramElementChange(possibleToChange){
         });
     }
 }
+function luminanace(r, g, b) {
+	//KS: source https://stackoverflow.com/questions/9733288/how-to-programmatically-calculate-the-contrast-ratio-between-two-colors
+    var a = [r, g, b].map(function (v) {
+        v /= 255;
+        return v <= 0.03928
+            ? v / 12.92
+            : Math.pow( (v + 0.055) / 1.055, 2.4 );
+    });
+    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
+function contrast(rgb1, rgb2) {
+    return (luminanace(rgb1[0], rgb1[1], rgb1[2]) + 0.05)
+         / (luminanace(rgb2[0], rgb2[1], rgb2[2]) + 0.05);
+}
