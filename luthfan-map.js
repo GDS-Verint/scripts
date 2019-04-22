1f8c91b046c01049e698fab09730628041b64e87
elduinn <luthfan.bumi@gmail.com>
2019-04-22 16:18:43 +0700

    Update luthfan-map.js

--- luthfan-map.js
+++ luthfan-map.js
@@ -37,6 +37,58 @@ var streetAddress='';
 
 var sx_customer_id='';
 
+var mapParams= {
+    extent:[334905.5753111506,
+    310733.193633054,
+    680181.2782575564,
+    663544.2449834899],
+    WKID:27700,
+    WKIDProj4:"+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs",
+    geolocateButton:!0,
+    geolocateAuto:!1,
+    geolocateWKID:4326,
+    geolocateWKIDProj4:"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs",
+    centerLonLat: {
+        x: 325226.83303699945, y: 673836.5572347812
+    }
+    ,
+    centerZoom:1,
+    geometryServer:"https://edinburghcouncilmaps.info/arcgis/rest/services/Utilities/Geometry/GeometryServer",
+    drawWindowMultiplier:1.25,
+    selectMultiple:!0,
+    selectStorage:[ {
+        points:[],
+        uniqueField:"FEATURE_ID",
+        selectedAssets:["RHF12",
+        "RHF10",
+        "RHF24"],
+        selectSymbol: {
+            color:[4,
+            4,
+            100],
+            size:"5",
+            outline: {
+                color: [100, 6, 6], width: "1"
+            }
+        }
+        ,
+        wkid:27700
+    }
+    ],
+    searchURL: {
+        base: "https://edinburghcouncilmaps.info/locatorhub/arcgis/rest/services/CAG/POSTCODE/GeocodeServer/findAddressCandidates?&SingleLine=&Fuzzy=true&outFields=*&maxLocations=2000&f=pjson&outSR=27700", varParams: ["LH_POSTCODE"]
+    }
+    ,
+    baseLayerService:"https://edinburghcouncilmaps.info/arcgis/rest/services/Basemaps/BasemapColour/MapServer/find",
+    backgroundMapService:"https://edinburghcouncilmaps.info/arcgis/rest/services/Basemaps/BasemapColour/MapServer",
+    addressSearchService: {
+        base: "https://edinburghcouncilmaps.info/locatorhub/arcgis/rest/services/CAG/ADDRESS/GeocodeServer/reverseGeocode?distance=300&outSR=27700&f=json"
+    }
+    ,
+    processResultURL:"https://my.edinburgh.gov.uk/ci/AjaxCustom/cagSearch"
+};
+
+var site_name_temp = '';
 var specifics = {};//KS allows form to override defaults
 function getSpecifics(){return specifics;}//KS accessor to give a level of abstraction when we need it
 function setSpecifics(obj){specifics = obj;}
@@ -49,6 +101,44 @@ var mapGlobal = {
 	centerLonLat: {x:325226.83303699945, y:673836.5572347812},
 	centerZoom: 1,
 }
+
+/*
+function parseRSMarker(e) {
+    var a = getWindowDrawBounds({
+        xMax: esrimap.extent.xmax,
+        yMax: esrimap.extent.ymax,
+        xMin: esrimap.extent.xmin,
+        yMin: esrimap.extent.ymin
+    });
+    console.log("1"), $.each(e.data, function(e, t) {
+        if (a.xMax >= t.longitude && a.yMax >= t.latitude && a.xMin <= t.longitude && a.yMin <= t.latitude) {
+            var r = new Point(Number(t.longitude), Number(t.latitude), new esri.SpatialReference({
+                    wkid: getMapParams().WKID
+                })),
+                o = new PictureMarkerSymbol(t.icon, 64, 64);
+            o.setOffset(0, 0);
+            var s = new Graphic(r, o);
+            s.setAttributes({
+                title: "",
+                description: t.description
+            }), esrimap.graphics.add(s), $(formName()).trigger("_map_caseMarkerDisplayed", [t.longitude, t.latitude, getMapParams().WKID, t.description])
+        }
+    }), console.log("2")
+}
+    */
+function getWindowDrawBounds(e) {
+    var a = 1;
+    return null !== getMapParams().drawWindowMultiplier && (a = getMapParams().drawWindowMultiplier), {
+        xMax: e.xMax + (a - 1) * (e.xMax - e.xMin),
+        yMax: e.yMax + (a - 1) * (e.yMax - e.yMin),
+        xMin: e.xMin - (a - 1) * (e.xMax - e.xMin),
+        yMin: e.yMin - (a - 1) * (e.yMax - e.yMin)
+    }
+}
+
+
+function getMapParams(){return mapParams}
+
 
 
  function parseRSMarker(response){
@@ -63,34 +153,31 @@ var mapGlobal = {
             caseLayer = new esri.layers.GraphicsLayer({id:"case_marker_layer"});
               $.each(response.data, function( i, result ) {
                   
-                 if (xmax >= result.longitude && ymax >= result.latitude && xmin <= result.longitude && ymin <= result.latitude && KDF.getVal('txt_event') && KDF.getVal('txt_eventcode') == $(result.description)[11].textContent) {
                      var point = new Point(Number(result.longitude), Number(result.latitude), new esri.SpatialReference({ wkid: 27700 }));
     				 var markerSymbol = new PictureMarkerSymbol(result.icon, 64, 64);
-    				 
+    			
     				 markerSymbol.setOffset(0, 0);
     				 var caseGraphic = new Graphic(point, markerSymbol);
-    				 caseGraphic.setAttributes({"title":'', "description": result.description});
+    				 caseGraphic.setAttributes({"title":'', "description": result.description, "caseid": result.title});
     				 //console.log(result.description);
     				 //esrimap.graphics.add(marker);
                      caseLayer.add(caseGraphic);
-                 }
+
              });
-             console.log(caseLayer)
+             //console.log(caseLayer)
                caseLayer.on('click', function(event) {
-               //console.log('mantap2');
-    				if (typeof esrimap.getLayer("graphicsLayer2") !== 'undefined') {
-                        esrimap.removeLayer(esrimap.getLayer("graphicsLayer2"));
-                        console.log('mantap2');
-                    }
+                  console.log(event.graphic.attributes.caseid)
+                  KDF.setVal('txt_case_id_subscribe', event.graphic.attributes.caseid)
+    		   if (typeof esrimap.getLayer("graphicsLayer2") !== 'undefined') {
+                   esrimap.getLayer("graphicsLayer2").hide();
+               }
     			});
-    			
     			esrimap.addLayer(caseLayer);
-           
              }); 
             
-            console.log('esrimap length boys' + esrimap.graphicsLayerIds.length)
+            //console.log('esrimap length boys' + esrimap.graphicsLayerIds.length)
             //esrimap.reorderLayer(esrimap.getLayer("case_marker_layer"), esrimap.graphicsLayerIds.length + 1);
-             console.log(esrimap)
+             //console.log(esrimap)
 }
 
 //testing only
@@ -108,9 +195,11 @@ $(document).on('keypress','#dform_widget_txt_postcode',function() {
   }
 });
 
-// Jquery event delegate so when the confirm location button clicked it can call another function
+// let's get back to this after the meeting
 $(document).on('click','.mapConfirm',function() {
     KDF.setVal('txt_issuestreet',KDF.getVal('le_gis_rgeo_desc'));
+    //call process adapter
+   // KDF.customdata('get_street_id', 'create', true, true, {'street_name': site_name_temp});
     KDF.gotoNextPage();
  });
  
@@ -246,7 +335,7 @@ function zoomChanged(evt){
     
 	//drawAssetLayer();
 	//console.log(evt)
-	console.log('Zoom: '+evt.lod.level)
+	//console.log('Zoom: '+evt.lod.level)
 	//KS won't be implemented due to demo
 	//Should we prevent clicking?
 	if (specifics.assetMaxDrawZoom){
@@ -265,10 +354,15 @@ function zoomChanged(evt){
 	    //KS implement default zoom extent
 	    if (evt.lod.level >= 6){
 	        
+	        var xmaxE = esrimap.extent.xmax;
+            var ymaxE = esrimap.extent.ymax;
+            var xminE = esrimap.extent.xmin;
+            var yminE = esrimap.extent.ymin;
+	        
 	    //console.log('zoom under default');
 	    //KS zoom is at or more magnified than max level - so draw it
 	        drawAssetLayer();
-	        getOpenCaseMarker();
+	        getOpenCaseMarker(xmaxE, xminE, ymaxE, yminE);
 	     
 		 
 	    }else{
@@ -313,6 +407,8 @@ function drawAssetLayer(){
     var ymaxE = esrimap.extent.ymax;
     var xminE = esrimap.extent.xmin;
     var yminE = esrimap.extent.ymin;
+    
+    //console.log(xmaxE + ' ' + ymaxE + ' ' + xminE + ' ' + yminE);
     
     var infoWindowContent;
     
@@ -376,17 +472,30 @@ function drawAssetLayer(){
 	        
 	        var point = new Point(Number(value.geometry.x), Number(value.geometry.y), new esri.SpatialReference({ wkid: 27700 }));
 	        var graphic = new esri.Graphic(point, sms);  
-	        graphic.setAttributes({"title": '', "description": infoWindowContent});
+	         if (KDF.getVal('asset_layer')) {
+	              graphic.setAttributes({"title": '', "description": infoWindowContent, "site_name":value.attributes.site_name, "latitude":value.geometry.y, "longitude":value.geometry.x});
+	        } else {
+                 graphic.setAttributes({"title": '', "description": infoWindowContent, "site_name": value.attributes.SITE_NAME, "latitude":value.geometry.y, "longitude":value.geometry.x});
+	        }
+	     
 		    assetLayer.add(graphic);
 	    });
 	    
 	         // assign the click event to the 
     	     assetLayer.on('click', function(event) {
+    	         console.log(event.graphic.attributes)
+    	         
+    	         site_name_temp = event.graphic.attributes.site_name;
     				//console.log(event.mapPoint.x);
     				if (typeof esrimap.getLayer("graphicsLayer2") !== 'undefined') {
                         esrimap.removeLayer(esrimap.getLayer("graphicsLayer2"));
                     }
                     
+                    var lan = event.graphic.attributes.latitude ;
+                    var long =  event.graphic.attributes.longitude ;
+                    
+                     KDF.customdata('reverse-geocode-edinburgh', 'create', true, true, {'longitude': long.toString() , 'latitude' : lan.toString()});
+                    KDF.unlock();
                    // esrimap.centerAt(new Point(event.mapPoint.x, event.mapPoint.y, new esri.SpatialReference({ wkid: 27700 })));
     			});
 
@@ -409,13 +518,16 @@ function drawAssetLayer(){
 $(document).on('change','#dform_widget_fault_reporting_search_results' , function() {
  
   var selectResult = $('select#dform_widget_fault_reporting_search_results option:checked').val();
-  console.log(faultReportingSearchResults);
+  //console.log(faultReportingSearchResults);
    if (selectResult !== "") {
-          $.each(faultReportingSearchResults, function(
-            key,
-            faultReportingSearchResults
-          ) {
+          $.each(faultReportingSearchResults, function(key,faultReportingSearchResults) {
             if (selectResult == faultReportingSearchResults.site_name) {
+                
+                      //get usrn after user do search
+    	              if (typeof KDF.getVal('txt_usrn') !== 'undefined') {
+    	                  KDF.setVal('txt_usrn', faultReportingSearchResults.USRN);
+    	              }
+	                  
                 
                   if (faultReportingSearchResults.east !==undefined && faultReportingSearchResults.east !=='' && faultReportingSearchResults.north !==undefined && faultReportingSearchResults.north !==''){
     							console.log(faultReportingSearchResults.north);
@@ -528,7 +640,7 @@ function processResult(searchInput){
 	$.ajax({url: 'https://my.edinburgh.gov.uk/ci/AjaxCustom/cagSearch',  crossDomain: true, method: 'POST',
 	data: {w_id: '6', search_term: searchInput, search_type: 'street'}
 	}).done(function(response) {
-	    //console.log(response);
+	   console.log(response);
 	   	if(response.length === 0){
     		KDF.showWidget('html_nosearchfound');
     		hideLoading();
@@ -541,6 +653,7 @@ function processResult(searchInput){
                 resultAssetArray[value.Abbreviation]['ymax'] = value.Street_Start_Y;
                 resultAssetArray[value.Abbreviation]['xmin']= value.Street_End_X;
                 resultAssetArray[value.Abbreviation]['ymin']= value.Street_End_Y;
+                resultAssetArray[value.Abbreviation]['USRN']= value.USRN;
           
 	        });
 	        
@@ -551,28 +664,32 @@ function processResult(searchInput){
 	             resultCount++;
 	          });
 	          
-	          console.log(resultCount);
-	          //console.log(resultAssetArray[value.Abbreviation].xmax);
+	          //console.log(resultCount);
 	          
 	          if(resultCount == 1){
+	         
 	              
 	              var xmax, xmin, ymax, ymin;
-	              console.log('haha');
 	              $.each(resultAssetArray, function(key, resultAssetArray ) {
-	                  console.log(resultAssetArray.xmax);
+	                  //console.log(resultAssetArray.xmax);
+	                  
+    	              //get usrn after user do search
+    	              if (typeof KDF.getVal('txt_usrn') !== 'undefined') {
+    	                  KDF.setVal('txt_usrn', resultAssetArray.USRN);
+    	              }
 	                  
 	                  xmax = parseFloat(resultAssetArray.xmax);
 	                  xmin = parseFloat(resultAssetArray.xmin);
 	                  ymax = parseFloat(resultAssetArray.ymax);
 	                  ymin = parseFloat(resultAssetArray.ymin);
 	              });
-	                console.log(xmin + ' ' + ymin + ' ' + xmax + ' '+ ymax);
+	                //console.log(xmin + ' ' + ymin + ' ' + xmax + ' '+ ymax);
 	              centreOnEsriResult('', '', xmax, xmin, ymax, ymin, '', '');
 	          } else {
 	               $('label[for=dform_widget_fault_reporting_search_results]').html('<label for="dform_widget_fault_reporting_search_results">Multiple results, please select one:</label>');
 	               $('#dform_widget_fault_reporting_search_results').append($('<option>', {value: 'Please select a location',text: 'Please select'}))
 	               $.each(resultAssetArray, function(key, resultAssetArray2 ) {
-	                   console.log(resultAssetArray2.site_name);
+	                   //console.log(resultAssetArray2.site_name);
     		    	   $('#dform_widget_fault_reporting_search_results').append($('<option>', {value: resultAssetArray2.site_name,text: resultAssetArray2.site_name}))
     		        
     		           KDF.showWidget('fault_reporting_search_results');
@@ -609,7 +726,7 @@ function setInfoWindowContent(content, xcoord, ycoord) {
 }
 	
 function showLoading(){
-    console.log('call a')
+    //console.log('call a')
 	KDF.showWidget('ahtm_cool_loading_gif');
 	changeAllLayersOpacity('0.2');
 	esrimap.disableMapNavigation();
@@ -617,7 +734,7 @@ function showLoading(){
 }
 
 function hideLoading(error){
-    console.log('call b')
+   // console.log('call b')
 	KDF.hideWidget('ahtm_cool_loading_gif');
 	changeAllLayersOpacity('0.9');
 	esrimap.enableMapNavigation();
@@ -696,9 +813,9 @@ function centreOnEsriResult(geometryX, geometryY, xmax, xmin, ymax, ymin, north,
 		esrimap.addLayer(newlayer);
     } else if (ymin!='') {
         
-        console.log('ymin dipanggil');
+        //console.log('ymin dipanggil');
         
-        console.log(xmin + ' ' + ymin + ' ' + xmax + ' '+ ymax);
+       // console.log(xmin + ' ' + ymin + ' ' + xmax + ' '+ ymax);
 		  var spatialref = new esri.SpatialReference({ wkid: 27700 }); //ref to British national grid projected coordinates
 		  var selectedExtent = new esri.geometry.Extent(xmax,ymax,xmin,ymin,spatialref);
 		  esrimap.setExtent(selectedExtent);
@@ -839,10 +956,10 @@ function addGeolocateButton(le_gis){
 	});
 }
 
-function getOpenCaseMarker(){
+function getOpenCaseMarker(xmax, xmin, ymax, ymin){
        if (KDF.getVal('txt_eventcode')) { 
                  
-                 console.log('mantap')
+                 //console.log('mantap')
                  //console.log(KDF.getVal('txt_customer_id'))
                  if (typeof KDF.getVal('txt_customer_id') !== undefined && KDF.getVal('txt_customer_id') !== "") {
                    sx_customer_id = KDF.getVal('txt_customer_id'); 
@@ -852,7 +969,8 @@ function getOpenCaseMarker(){
                      //console.log('assdf11df')
                  }
                  //console.log(sx_customer_id);
-                KDF.customdata('open_case_marker', 'create', true, true, {'eventcode': KDF.getVal('txt_eventcode'),'customerid': sx_customer_id});
+               KDF.customdata('open_case_marker', 'create', true, true, {'eventcode': KDF.getVal('txt_eventcode'),'customerid': sx_customer_id, 'xmax': xmax.toString(), 'xmin':xmin.toString(), 'ymax': ymax.toString(), 'ymin': ymin.toString()});
+                //KDF.customdata('get_open_case_marker', 'create', true, true, {'eventcode': KDF.getVal('txt_eventcode')});
                 KDF.unlock();
              }   
 }
