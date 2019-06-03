4140bb7c18f2cb2310c454db1056d44927cbd4f2
GDS-Verint <45266375+GDS-Verint@users.noreply.github.com>
2019-06-03 13:50:34 +0100

    Update style-4.js

--- style-4.js
+++ style-4.js
@@ -6,6 +6,9 @@ applyStyle(['recommended']);
 function defineDefaultStyle(){
 	//KS: temp
 	regexSearch("[0-9A-Za-z ]{2,}");
+	console.log($('.pc_regex').length + ' elements appling postcode regex to')
+	regexSearch('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$',
+		    '.pc_regex');
 	//KS: can define listeners here, but can't later on, need to call 
     //KS: adds the recommended default styling - and acts a single location to change them
 	//KS: for the love of StackExchange don't put 'all' or 'recommended' in here
