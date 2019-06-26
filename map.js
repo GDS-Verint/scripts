ada3507c79df4043059a3ad57385b463ff482ea4
GDS-Verint <45266375+GDS-Verint@users.noreply.github.com>
2019-06-26 10:02:32 +0100

    Update map.js

--- map.js
+++ map.js
@@ -329,7 +329,7 @@ function luthfanDrawAssetLayer(){//TODO update URL
 	}).fail(function() {
 	    KDF.showError('It looks like the connection to our mapping system has failed, please try to log the fault again');
 	});
-  }else{esrimap.removeLayer(esrimap.getLayer('case_marker_layer'));}
+  }else if(esrimap.getLayer('case_marker_layer') !== undefined){esrimap.removeLayer(esrimap.getLayer('case_marker_layer'));}
 }
 
 function keatonDrawAssetLayer(layersToKeep){
